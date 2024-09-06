5e23aa77522294bfe83ad63e9b2515b0312bfba3
Eric Ihli <eihli@owoga.com>
2024-09-06 10:58:48 -0700

    alignment

--- code/via.s
+++ code/via.s
@@ -1,7 +1,7 @@
     .ifndef PORTA
-PORTB	=	$6000
-PORTA	=	$6001
-DDRB    =   $6002
-DDRA   	=	$6003
+PORTB = $6000
+PORTA = $6001
+DDRB  = $6002                   ; Data Direction Register for Port B
+DDRA  = $6003                   ; Data Direction Register for Port A
     .endif
 
